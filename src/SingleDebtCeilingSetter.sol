e80c6d721a361ad66531fcab9f8789e950fdb064
stefanionescu <stefanionescu@protonmail.com>
2021-01-09 01:21:48 +0000

    Implemented single debt ceiling setter

--- src/SingleDebtCeilingSetter.sol
+++ src/SingleDebtCeilingSetter.sol
@@ -1,34 +1,28 @@
 pragma solidity ^0.6.7;
 
+import "geb-treasury-reimbursement/IncreasingTreasuryReimbursement.sol";
+
 abstract contract SAFEEngineLike {
+    function collateralTypes(bytes32) virtual public view returns (
+        uint256 debtAmount,        // [wad]
+        uint256 accumulatedRate,   // [ray]
+        uint256 safetyPrice,       // [ray]
+        uint256 debtCeiling        // [rad]
+    );
+    function globalDebtCeiling() virtual public view returns (uint256);
+    function modifyParameters(
+        bytes32 parameter,
+        uint256 data
+    ) virtual external;
     function modifyParameters(
         bytes32 collateralType,
         bytes32 parameter,
         uint256 data
-    ) external;
-}
-abstract contract StabilityFeeTreasuryLike {
-    function getAllowance(address) virtual external view returns (uint, uint);
-    function systemCoin() virtual external view returns (address);
-    function pullFunds(address, address, uint) virtual external;
+    ) virtual external;
 }
 
-contract SingleDebtCeilingSetter {
+contract SingleDebtCeilingSetter is IncreasingTreasuryReimbursement {
     // --- Auth ---
-    mapping (address => uint256) public authorizedAccounts;
-    function addAuthorization(address account) external isAuthorized {
-        authorizedAccounts[account] = 1;
-        emit AddAuthorization(account);
-    }
-    function removeAuthorization(address account) external isAuthorized {
-        authorizedAccounts[account] = 0;
-        emit RemoveAuthorization(account);
-    }
-    modifier isAuthorized {
-        require(authorizedAccounts[msg.sender] == 1, "SingleDebtCeilingSetter/account-not-authorized");
-        _;
-    }
-
     mapping (address => uint256) public manualSetters;
     function addManualSetter(address account) external isAuthorized {
         manualSetters[account] = 1;
@@ -46,41 +40,24 @@ contract SingleDebtCeilingSetter {
     // --- Variables ---
     // The max amount of system coins that can be generated using this collateral type
     uint256 public maxCollateralCeiling;            // [rad]
+    // The min amount of system coins that must be generated using this collateral type
+    uint256 public minCollateralCeiling;            // [rad]
     // Percentage change applied to the collateral's debt ceiling
     uint256 public ceilingPercentageChange;         // [hundred]
     // When the price feed was last updated
     uint256 public lastUpdateTime;                  // [timestamp]
     // Enforced gap between calls
     uint256 public updateDelay;                     // [seconds]
-    // Starting reward for the feeReceiver of a updateRate call
-    uint256 public baseUpdateCallerReward;          // [wad]
-    // Max possible reward for the feeReceiver of a updateRate call
-    uint256 public maxUpdateCallerReward;           // [wad]
-    // Max delay taken into consideration when calculating the adjusted reward
-    uint256 public maxRewardIncreaseDelay;          // [seconds]
-    // Rate applied to baseUpdateCallerReward every extra second passed beyond updateDelay seconds since the last updateRate call
-    uint256 public perSecondCallerRewardIncrease;   // [ray]
     // The collateral's name
     bytes32 public collateralName;
 
     // The SAFEEngine contract
     SAFEEngineLike public safeEngine;
-    // The treasury contract
-    StabilityFeeTreasuryLike public treasury;
 
     // --- Events ---
-    event AddAuthorization(address account);
-    event RemoveAuthorization(address account);
     event AddManualSetter(address account);
     event RemoveManualSetter(address account);
-    event ModifyParameters(
-        bytes32 parameter,
-        uint256 val
-    );
-    event ModifyParameters(
-        bytes32 parameter,
-        address addr
-    );
+    event UpdateCeiling(uint256 nextCeiling);
 
     constructor(
       address safeEngine_,
@@ -91,45 +68,33 @@ contract SingleDebtCeilingSetter {
       uint256 perSecondCallerRewardIncrease_,
       uint256 updateDelay_,
       uint256 ceilingPercentageChange_,
-      uint256 maxCollateralCeiling_
-    ) public {
+      uint256 maxCollateralCeiling_,
+      uint256 minCollateralCeiling_
+    ) public IncreasingTreasuryReimbursement(treasury_, baseUpdateCallerReward_, maxUpdateCallerReward_, perSecondCallerRewardIncrease_) {
         require(safeEngine_ != address(0), "SingleDebtCeilingSetter/invalid-safe-engine");
-        if (address(treasury_) != address(0)) {
-          require(StabilityFeeTreasuryLike(treasury_).systemCoin() != address(0), "SingleDebtCeilingSetter/treasury-coin-not-set");
-        }
-        require(maxUpdateCallerReward_ >= baseUpdateCallerReward_, "SingleDebtCeilingSetter/invalid-max-caller-reward");
-        require(perSecondCallerRewardIncrease_ >= RAY, "SingleDebtCeilingSetter/invalid-per-second-reward-increase");
-        require(updateDelay_ > 0, "SingleDebtCeilingSetter");
+        require(updateDelay_ > 0, "SingleDebtCeilingSetter/invalid-update-delay");
         require(both(ceilingPercentageChange_ > HUNDRED, ceilingPercentageChange_ <= THOUSAND), "SingleDebtCeilingSetter/invalid-percentage-change");
-        require(maxCollateralCeiling_ > 0, "SingleDebtCeilingSetter/invalid-max-ceiling");
+        require(minCollateralCeiling_ > 0, "SingleDebtCeilingSetter/invalid-min-ceiling");
+        require(both(maxCollateralCeiling_ > 0, maxCollateralCeiling_ > minCollateralCeiling_), "SingleDebtCeilingSetter/invalid-max-ceiling");
 
-        authorizedAccounts[msg.sender]  = 1;
-        safeEngine                      = SAFEEngineLike(safeEngine_);
-        treasury                        = StabilityFeeTreasuryLike(treasury_);
-        collateralName                  = collateralName_;
-        baseUpdateCallerReward          = baseUpdateCallerReward_;
-        maxUpdateCallerReward           = maxUpdateCallerReward_;
-        perSecondCallerRewardIncrease   = perSecondCallerRewardIncrease_;
-        updateDelay                     = updateDelay_;
-        ceilingPercentageChange         = ceilingPercentageChange_;
-        maxCollateralCeiling            = maxCollateralCeiling_;
-        maxRewardIncreaseDelay          = uint(-1);
+        manualSetters[msg.sender] = 1;
 
-        emit AddAuthorization(msg.sender);
-        emit ModifyParameters("treasury", treasury_);
-        emit ModifyParameters("baseUpdateCallerReward", baseUpdateCallerReward);
-        emit ModifyParameters("maxUpdateCallerReward", maxUpdateCallerReward);
-        emit ModifyParameters("perSecondCallerRewardIncrease", perSecondCallerRewardIncrease);
+        safeEngine                = SAFEEngineLike(safeEngine_);
+        collateralName            = collateralName_;
+        updateDelay               = updateDelay_;
+        ceilingPercentageChange   = ceilingPercentageChange_;
+        maxCollateralCeiling      = maxCollateralCeiling_;
+        minCollateralCeiling      = minCollateralCeiling_;
+
         emit ModifyParameters("updateDelay", updateDelay);
         emit ModifyParameters("ceilingPercentageChange", ceilingPercentageChange);
         emit ModifyParameters("maxCollateralCeiling", maxCollateralCeiling);
-        emit ModifyParameters("maxRewardIncreaseDelay", maxRewardIncreaseDelay);
+        emit ModifyParameters("minCollateralCeiling", minCollateralCeiling);
     }
 
     // --- Math ---
     uint256 constant HUNDRED  = 100;
     uint256 constant THOUSAND = 1000;
-    uint256 constant RAY      = 10**27;
 
     // --- Boolean Logic ---
     function either(bool x, bool y) internal pure returns (bool z) {
@@ -173,7 +138,12 @@ contract SingleDebtCeilingSetter {
           updateDelay = val;
         }
         else if (parameter == "maxCollateralCeiling") {
+          require(both(maxCollateralCeiling > 0, maxCollateralCeiling > minCollateralCeiling), "SingleDebtCeilingSetter/invalid-max-ceiling");
           maxCollateralCeiling = val;
+        }
+        else if (parameter == "minCollateralCeiling") {
+          require(minCollateralCeiling > 0, "SingleDebtCeilingSetter/invalid-min-ceiling");
+          minCollateralCeiling = val;
         }
         else if (parameter == "ceilingPercentageChange") {
           require(both(val > HUNDRED, val <= THOUSAND), "SingleDebtCeilingSetter/invalid-percentage-change");
@@ -190,50 +160,59 @@ contract SingleDebtCeilingSetter {
         );
     }
 
-    // --- Treasury ---
-    /**
-    * @notice This returns the stability fee treasury allowance for this contract by taking the minimum between the per block and the total allowances
-    **/
-    function treasuryAllowance() public view returns (uint256) {
-        (uint total, uint perBlock) = treasury.getAllowance(address(this));
-        return minimum(total, perBlock);
-    }
-    /*
-    * @notice Get the SF reward that can be sent to the updateRate() caller right now
-    */
-    function getCallerReward() public view returns (uint256) {
-        uint256 timeElapsed = (lastUpdateTime == 0) ? updateDelay : subtract(now, lastUpdateTime);
-        if (timeElapsed < updateDelay) {
-            return 0;
+    // --- Utils ---
+    function setCeiling(uint256 nextDebtCeiling) internal {
+        (uint256 debtAmount, uint256 accumulatedRate, uint256 safetyPrice, uint256 currentDebtCeiling) = safeEngine.collateralTypes(collateralName);
+
+        if (safeEngine.globalDebtCeiling() < nextDebtCeiling) {
+            safeEngine.modifyParameters("globalDebtCeiling", nextDebtCeiling);
         }
-        uint256 adjustedTime = subtract(timeElapsed, updateDelay);
-        uint256 maxReward    = minimum(maxUpdateCallerReward, treasuryAllowance() / RAY);
-        if (adjustedTime > maxRewardIncreaseDelay) {
-            return maxReward;
-        }
-        uint256 baseReward   = baseUpdateCallerReward;
-        if (adjustedTime > 0) {
-            baseReward = rmultiply(rpower(perSecondCallerRewardIncrease, adjustedTime, RAY), baseReward);
-        }
-        if (baseReward > maxReward) {
-            baseReward = maxReward;
-        }
-        return baseReward;
-    }
-    /**
-    * @notice Send a stability fee reward to an address
-    * @param proposedFeeReceiver The SF receiver
-    * @param reward The system coin amount to send
-    **/
-    function rewardCaller(address proposedFeeReceiver, uint256 reward) internal {
-        if (address(treasury) == proposedFeeReceiver) return;
-        if (address(treasury) == address(0) || reward == 0) return;
-        address finalFeeReceiver = (proposedFeeReceiver == address(0)) ? msg.sender : proposedFeeReceiver;
-        try treasury.pullFunds(finalFeeReceiver, treasury.systemCoin(), reward) {}
-        catch(bytes memory revertReason) {
-            emit FailRewardCaller(revertReason, finalFeeReceiver, reward);
+
+        if (currentDebtCeiling != nextDebtCeiling) {
+            safeEngine.modifyParameters(collateralName, "debtCeiling", nextDebtCeiling);
+            emit UpdateCeiling(nextDebtCeiling);
         }
     }
 
-    
+    // --- Auto Updates ---
+    function autoUpdateCeiling(address feeReceiver) external {
+        // Check that the update time is not in the future
+        require(lastUpdateTime < now, "SingleDebtCeilingSetter/update-time-in-the-future");
+        // Check delay between calls
+        require(either(subtract(now, lastUpdateTime) >= updateDelay, lastUpdateTime == 0), "SingleDebtCeilingSetter/wait-more");
+
+        // Get the caller's reward
+        uint256 callerReward = getCallerReward(lastUpdateTime, updateDelay);
+        // Update lastUpdateTime
+        lastUpdateTime = now;
+
+        // Get the next ceiling and set it
+        uint256 nextCollateralCeiling = getNextCollateralCeiling();
+        setCeiling(nextCollateralCeiling);
+
+        // Pay the caller for updating the rate
+        rewardCaller(feeReceiver, callerReward);
+    }
+
+    // --- Manual Updates ---
+    function manualUpdateCeiling() external isManualSetter {
+        uint256 nextCollateralCeiling = getNextCollateralCeiling();
+        setCeiling(nextCollateralCeiling);
+    }
+
+    // --- Getters ---
+    function getNextCollateralCeiling() public view returns (uint256) {
+        (uint256 debtAmount, uint256 accumulatedRate, uint256 safetyPrice, uint256 currentDebtCeiling) = safeEngine.collateralTypes(collateralName);
+        uint256 adjustedCurrentDebt = multiply(debtAmount, accumulatedRate);
+
+        if (either(currentDebtCeiling < minCollateralCeiling, debtAmount == 0)) return minCollateralCeiling;
+        else if (currentDebtCeiling >= maxCollateralCeiling) return maxCollateralCeiling;
+
+        uint256 updatedCeiling = multiply(adjustedCurrentDebt, ceilingPercentageChange) / HUNDRED;
+
+        if (updatedCeiling < minCollateralCeiling) return minCollateralCeiling;
+        else if (updatedCeiling > maxCollateralCeiling) return maxCollateralCeiling;
+
+        return updatedCeiling;
+    }
 }
